c566eb9875d7949f00eb3342837896d02a045482
abdulrhmaanm <abdulrhmanmahmod77@icloud.com>
2025-07-25 18:19:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Portfolio from './Portfolio';
-import Footer from './footer';
+import Footer from './Footer';
 
 const router = createBrowserRouter([
   {
